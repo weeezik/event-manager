385308519e0fd3c68454a272df542c0565b439ff
weeezik <mkihanya.33@gmail.com>
2024-07-04 15:15:42 -0700

    Add thank you save_thank_you_letter

---
+++ lib/save_thank_you_letter.rb
@@ -0,0 +1,7 @@
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
