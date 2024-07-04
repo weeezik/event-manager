c7665cb33138e7e969cf5f761e38eebbbea3002e
weeezik <mkihanya.33@gmail.com>
2024-07-04 10:07:57 -0700

    Use ruby CSV file parser on added event_attendees CSV file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,12 @@
+require 'csv'
 puts 'Event Manager Initialized!'
+
+contents = CSV.open('event_attendees.csv', 
+headers: true, 
+header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+  puts name
+end
+
